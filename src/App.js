c1cb875931295421cb8889723f100a448060784a
devraysingh <raysahni98@gmail.com>
2023-03-16 17:30:50 -0400

    Pages, Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Header from "./components/Header/Header";
+
 import HomePage from './pages/HomePage/HomePage';
 import StockPage from './pages/StockPage/StockPage';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
@@ -8,7 +8,7 @@ import './App.scss';
 function App() {
   return(
   <BrowserRouter>
-      <Header />
+   
       <Routes>
         <Route path="/" element={<HomePage />}/>
         <Route path="/:id" element={<StockPage />}/>
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import "./HomePage.scss";
+import { fetchMarketNews } from "../../api"
+import axios from 'axios';
+export default function HomePage() {
+
+    const [newsData, setNewsData] = useState({});
+
+    const apiKey = process.env.REACT_APP_API_KEY;
+    
+    const fetchMarketNews = async () => {
+        try {
+            const response = await axios.get(`https://www.alphavantage.co/query?function=NEWS_SENTIMENT&apikey=${apiKey}`);
+            console.log(response.data)
+            setNewsData(response.data);        
+        } catch (e) {
+            console.error('Error 404: Fetching Market News:', e);
+        }
+    };
+    
+    
+    
+    
+
+
+
+    useEffect(() => {
+        fetchMarketNews();
+        console.log(newsData)
+    }, []);
+    
+
+    return (
+        <div className='home-page'>
+        </div>
+    )
+}
+
