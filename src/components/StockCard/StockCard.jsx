f67bf8dbc4442c41d4852e1699ae720f5bf22a1b
devraysingh <raysahni98@gmail.com>
2023-03-16 14:20:01 -0400

    Stock Card Setup, News Section Setup, Partials

---
+++ src/components/StockCard/StockCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Card(props) {
+    return (
+        <div className="stock-card">
+            <div className="stock-card__chart">
+
+            </div>
+            <div className="stock-card__stock-details">
+
+            </div>
+        </div>
+    )
+}
