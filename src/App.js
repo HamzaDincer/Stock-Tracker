72e96cd67eb426193fc49ce4f29b2d391369946f
Hamza Dincer <hamzamustakdincer@gmail.com>
2023-03-16 14:13:18 -0400

    workspace is ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={"logo"} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+  <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<HomePage />}/>
+        <Route path="/:id" element={<StockPage />}/>
+      </Routes>
+  </BrowserRouter>
+  ) ;
 }
-
 export default App;
